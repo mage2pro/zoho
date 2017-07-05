5e12e0a9bc9fc6bf01da675dfa3126296338ac3c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-05 09:17:36 +0300

    no message

---
+++ T/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+namespace Df\Zoho\T;
+/**
+ * 2017-07-05
+ * @see \Df\Zoho\T\Basic
+ * @see \Dfe\ZohoBooks\T\TestCase
+ * @see \Dfe\ZohoCRM\T\TestCase
+ * @see \Dfe\ZohoInventory\T\TestCase
+ */
+abstract class TestCase extends \Df\Core\TestCase {}

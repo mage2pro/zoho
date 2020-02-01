e6636671f484e4bb28dfd8fa58c7a9cdd3c3f8e5
Dmitry Fedyuk <dfediuk@gmail.com>
2020-02-01 10:27:40 +0600

    no message

--- Test/TestCase.php
+++ Test/TestCase.php
@@ -3,7 +3,7 @@ namespace Df\Zoho\Test;
 /**
  * 2017-07-05
  * @see \Df\Zoho\Test\Basic
- * @see \Df\ZohoBI\T\TestCase
- * @see \Dfe\ZohoCRM\T\TestCase
+ * @see \Df\ZohoBI\Test\TestCase
+ * @see \Dfe\ZohoCRM\Test\TestCase
  */
 abstract class TestCase extends \Df\Core\TestCase {}

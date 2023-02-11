0d72238f8355e8d25708ceb67ca367feef3751f3
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-11 03:12:08 +0000

    https://github.com/mage2pro/core/issues/168

--- lib/main.php
+++ lib/main.php
@@ -4,6 +4,5 @@ use Df\Zoho\App;
  * 2017-07-06
  * @used-by \Df\ZohoBI\Source\Organization::app()
  * @param string|object $c
- * @return App
  */
-function df_zoho_app($c) {return App::s($c);}
+function df_zoho_app($c):App {return App::s($c);}
